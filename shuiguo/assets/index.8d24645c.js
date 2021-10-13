9494853d3f46326e6fb30583df895519d74ad4d9
chinanf-boy <yobrave@outlook.com>
2021-10-14 00:58:07 +0800

    shuiguo: vue3 + vite2 + vuex4 + router4 + vant3

---
+++ shuiguo/assets/index.8d24645c.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.3f9869dd.js";import{o,e as r}from"./vendor.299a0f4c.js";const n={};function t(a,s,c,d,f,i){return o(),r("div",null," nihao ")}var l=e(n,[["render",t]]);export{l as default};
---
+++ shuiguo/assets/storage.691bfb87.js
@@ -0,0 +1,1 @@
+const o=t=>localStorage.getItem(t),a=(t,e)=>{localStorage.setItem(t,e)};export{o as g,a as s};
